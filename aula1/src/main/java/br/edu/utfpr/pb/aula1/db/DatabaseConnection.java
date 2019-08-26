0e640d8dd325724df19bef6863ab42b774b4fe79
Vinicius Pegorini <viniciuspegorini@gmail.com>
2019-08-26 20:20:26 -0300

    2019-08-26 20h20 - aula2 - inicial

--- aula1/src/main/java/br/edu/utfpr/pb/aula1/db/DatabaseConnection.java
+++ aula1/src/main/java/br/edu/utfpr/pb/aula1/db/DatabaseConnection.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.utfpr.pb.aula1.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-/**
- *
- * @author Vinicius
- */
 public class DatabaseConnection {
     private static DatabaseConnection dbConnection;
     private Connection conn;
