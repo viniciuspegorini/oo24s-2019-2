03dc9567cb3b29ca7f0589191d8c18e139db00a3
Vinicius Pegorini <viniciuspegorini@gmail.com>
2019-10-14 20:15:27 -0300

    2019-10-14 - aula 3 - atualizando

--- aula3/src/main/java/br/edu/utfpr/pb/oo24s/aula3/controller/FXMLPrincipalController.java
+++ aula3/src/main/java/br/edu/utfpr/pb/oo24s/aula3/controller/FXMLPrincipalController.java
@@ -2,16 +2,64 @@ package br.edu.utfpr.pb.oo24s.aula3.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLPrincipalController implements Initializable {
 
+    @FXML
+    private TextField textField;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private CheckBox checkBox;
+    
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private Button button2;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // textField.setText("TEXTO adicionado no controller!!! ");
+        
+        this.button2.setOnAction(
+                (final ActionEvent e) -> {
+                    actionButton2();
+                }
+        );
     }    
     
+    @FXML
+    private void actionButton1() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(".: Exemplo JavaFX :.");
+        alert.setHeaderText("JavaFX");
+        alert.setContentText(
+                this.textField.getText() + "\n" + 
+                this.datePicker.getValue() + "\n" + 
+                (checkBox.isSelected() ? "Check: TRUE" : "Check: FALSE")
+        );
+        alert.showAndWait();
+    }
+
+    private void actionButton2() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(".: Exemplo JavaFX - Button 2 :.");
+        alert.setHeaderText("JavaFX");
+        alert.setContentText(
+                this.textArea.getText()
+        );
+        alert.showAndWait();
+    }
 }
