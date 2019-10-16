f1473a7273f3ad6ad6dd50e31326ed8a455d51b7
Vinicius Pegorini <viniciuspegorini@gmail.com>
2019-10-16 20:35:19 -0300

    2019-10-16 - aula4 - javaFX adicionado FXMLCategoriaLista

---
+++ aula4/src/main/java/br/edu/utfpr/pb/oo24s/aula4/controller/FXMLCategoriaListaController.java
@@ -0,0 +1,22 @@
+package br.edu.utfpr.pb.oo24s.aula4.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author vinic
+ */
+public class FXMLCategoriaListaController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
